2fc82ad68bca8f53cc949685144736a19decc152
Zhongyang Gao <billgao0807@gmail.com>
2016-02-01 23:15:58 -0800

    Final version

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -15,26 +15,32 @@ class PlaySoundsViewController: UIViewController {
   var audioPlayer:AVAudioPlayer!
   var receivedAudio:RecordedAudio!
   
+  //Declared globally within PlaySoundsViewController
+  var audioEngine:AVAudioEngine!
+  var audioFile:AVAudioFile!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-//    if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
-//      let filePathUrl = NSURL.fileURLWithPath(filePath)
-//      audioPlayer = try!
-//        AVAudioPlayer(contentsOfURL: filePathUrl)
-//      audioPlayer.enableRate = true
-//      
-//    } else {
-//      print("file path incorrect")
-//    }
+    //    if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+    //      let filePathUrl = NSURL.fileURLWithPath(filePath)
+    //      audioPlayer = try!
+    //        AVAudioPlayer(contentsOfURL: filePathUrl)
+    //      audioPlayer.enableRate = true
+    //
+    //    } else {
+    //      print("file path incorrect")
+    //    }
     audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
     audioPlayer.enableRate = true
+    
+    audioEngine = AVAudioEngine()
+    audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
     
   }
   
   func setRate(rate: Float) {
     audioPlayer.stop()
-    audioPlayer.currentTime = 0
     audioPlayer.rate = rate
     audioPlayer.play()
   }
@@ -56,10 +62,41 @@ class PlaySoundsViewController: UIViewController {
   
   @IBAction func stopPlayer(sender: UIButton) {
     audioPlayer.stop()
+    audioPlayer.currentTime = 0
   }
+  
+  @IBAction func playChipmunkAudio(sender: UIButton) {
+    playAudioWithVariablePitch(1000)
+  }
+  
+  @IBAction func playDarthvaderAudio(sender: UIButton) {
+    playAudioWithVariablePitch(-1000)
+  }
+  func playAudioWithVariablePitch(pitch: Float){
+    audioPlayer.stop()
+    audioEngine.stop()
+    audioEngine.reset()
+    
+    let audioPlayerNode = AVAudioPlayerNode()
+    audioEngine.attachNode(audioPlayerNode)
+    
+    let changePitchEffect = AVAudioUnitTimePitch()
+    changePitchEffect.pitch = pitch
+    audioEngine.attachNode(changePitchEffect)
+    
+    audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+    audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+    
+    audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+    try! audioEngine.start()
+    
+    audioPlayerNode.play()
+  }
+  
   /*
   // MARK: - Navigation
   
+
   // In a storyboard-based application, you will often want to do a little preparation before navigation
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
   // Get the new view controller using segue.destinationViewController.
